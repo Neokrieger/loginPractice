5dae9c456600dfa6f0b64936d3edfe9dc2cc88fe
Neokrieger <theneokrieger@hotmail.com>
2021-08-09 19:20:15 +0100

    Very good login

--- app.js
+++ app.js
@@ -10,16 +10,43 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded( { extended: true }))
 
 app.get('/login', async (req, res) => {
-	res.render('login');
+	res.render('login', {
+		error: ""
+	});
 });
 
-app.post('/logins', async (req, res) => {
+app.post('/login', async (req, res) =>{
+
+})
+
+app.post('/registered', async (req, res) => {
     await Login.create({
         username : req.body.username,
         password : req.body.password
     })
     res.redirect('/login')
 });
+
+app.post('/login/error', async (req, res) => {
+
+	const logins = await Login.findAll({})
+
+	logins.forEach(login => {
+		if(login.username === req.body.username && login.password === req.body.password){
+			res.redirect(`/user/${login.id}`)
+		}
+	})
+
+	  res.render(`login`, {
+			error: "Username/Password is incorrect"
+		});
+});
+
+app.get(`/user/:id`, async (req, res) => {
+
+	res.render('user');
+})
+
 
 app.get('/register', async (req, res) => {
 	res.render('register');
@@ -28,4 +55,3 @@ app.get('/register', async (req, res) => {
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
 })
-
